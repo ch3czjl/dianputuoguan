818bae6cd8a1236b3ad2babafbf231048146bc22
ch3czjl <598746155@qq.com>
2020-06-01 15:51:16 +0800

    chenggong

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -40,7 +40,7 @@ handle403 = 'blog.views.permission_denied_view'
 urlpatterns = [
     url(r'^admin/', admin_site.urls),
     # url(r'^dianptg/',dianptg.urls),
-    path('', include(dianptg.urls,namespace='dianptg')),
+    path('dianptg/', include('dianptg.urls')),
     url(r'', include('blog.urls', namespace='blog')),
     url(r'mdeditor/', include('mdeditor.urls')),
     url(r'', include('comments.urls', namespace='comment')),
